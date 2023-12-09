a25619d45d0588b4b16c9d6e954f5b9ffe8bf853
Sofia Weng <sowe0001@stud.kea.dk>
2023-12-09 17:10:53 +0100

    hover på facade virker

--- Script/script.js
+++ Script/script.js
@@ -11,3 +11,33 @@ document.querySelectorAll(".nav-link").forEach((n) =>
     navmenu.classList.remove("active");
   })
 );
+
+document
+  .querySelector("#menucontainer")
+  .addEventListener("mouseover", menu_mouseover);
+document
+  .querySelector("#menucontainer")
+  .addEventListener("mouseout", menu_mouseout);
+function menu_mouseover() {
+  console.log("menu_mouseover");
+  document.getElementById("menuhover").style.display = "inherit";
+}
+function menu_mouseout() {
+  console.log("menu_mouseout");
+  document.getElementById("menuhover").style.display = "none";
+}
+document
+  .querySelector("#cateringcontainer")
+  .addEventListener("mouseover", catering_mouseover);
+
+document
+  .querySelector("#cateringcontainer")
+  .addEventListener("mouseout", catering_mouseout);
+function catering_mouseover() {
+  console.log("catering_mouseover");
+  document.getElementById("cateringhover").style.display = "inherit";
+}
+function catering_mouseout() {
+  console.log("catering_mouseout");
+  document.getElementById("cateringhover").style.display = "none";
+}
