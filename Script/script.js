361c1f7e145122861e4448671cb278f776cb46b2
Sofia Weng <sowe0001@stud.kea.dk>
2023-12-09 18:11:11 +0100

    opdateret version af hover på facade

--- Script/script.js
+++ Script/script.js
@@ -11,13 +11,39 @@ document.querySelectorAll(".nav-link").forEach((n) =>
     navmenu.classList.remove("active");
   })
 );
+window.addEventListener("load", sidenVises);
+function sidenVises() {
+  console.log("sidenvises");
+  //tilføjer eventlistener på menu
+  document
+    .querySelector("#menucontainer")
+    .addEventListener("mouseover", menu_mouseover);
+  document
+    .querySelector("#menucontainer")
+    .addEventListener("mouseout", menu_mouseout);
+  document
+    .querySelector("#knapmenu")
+    .addEventListener("mouseover", menu_mouseover);
+  document
+    .querySelector("#knapmenu")
+    .addEventListener("mouseout", menu_mouseout);
 
-document
-  .querySelector("#menucontainer")
-  .addEventListener("mouseover", menu_mouseover);
-document
-  .querySelector("#menucontainer")
-  .addEventListener("mouseout", menu_mouseout);
+  //tilføjer eventlistener på catering
+  document
+    .querySelector("#cateringcontainer")
+    .addEventListener("mouseover", catering_mouseover);
+  document
+    .querySelector("#cateringcontainer")
+    .addEventListener("mouseout", catering_mouseout);
+  document
+    .querySelector("#knapcatering")
+    .addEventListener("mouseover", catering_mouseover);
+  document
+    .querySelector("#knapcatering")
+    .addEventListener("mouseout", catering_mouseout);
+}
+
+//menu
 function menu_mouseover() {
   console.log("menu_mouseover");
   document.getElementById("menuhover").style.display = "inherit";
@@ -26,13 +52,8 @@ function menu_mouseout() {
   console.log("menu_mouseout");
   document.getElementById("menuhover").style.display = "none";
 }
-document
-  .querySelector("#cateringcontainer")
-  .addEventListener("mouseover", catering_mouseover);
 
-document
-  .querySelector("#cateringcontainer")
-  .addEventListener("mouseout", catering_mouseout);
+//catering
 function catering_mouseover() {
   console.log("catering_mouseover");
   document.getElementById("cateringhover").style.display = "inherit";
