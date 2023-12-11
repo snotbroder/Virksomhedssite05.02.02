0e04905bf352f271d299d94db730a153849cb00a
Joris <jote0002@stud.kea.dk>
2023-12-11 09:50:07 +0100

    Ændret navn på test.js til menu.js. linket på menu.html

--- Script/menu.js
+++ Script/menu.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", sidenVises);
 
-//Kontanter
+//forkortelser
 let menu1 = document.querySelector("#menu1");
 let menu2 = document.querySelector("#menu2");
 let menu3 = document.querySelector("#menu3");
